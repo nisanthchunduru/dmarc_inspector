dfff2984575579a83dad0023b6e23250c1de5bd5
Nisanth Chunduru <nisanth074@gmail.com>
2014-08-25 16:45:53 +0530

    DMARCInspector#receiver_policy returns receiver policy of a domain

--- lib/dmarc_inspector.rb
+++ lib/dmarc_inspector.rb
@@ -1,9 +1,20 @@
 $: << File.dirname(__FILE__) unless $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require "dmarc_inspector/version"
+require 'resolv'
+require 'dmarc_inspector/dmarc/parser'
 
 class DMARCInspector
+  def initialize(domain)
+    @domain = domain
+  end
+
   def receiver_policy
-    :none
+    dmarc_subdomain = "_dmarc.#{@domain}"
+    dns = Resolv::DNS.new
+    dns_resource_data = dns.getresource(dmarc_subdomain, Resolv::DNS::Resource::IN::TXT).data
+
+    record = DMARC::Parser.parse(dns_resource_data)
+    record[:p]
   end
 end
--- spec/dmarc_inspector_spec.rb
+++ spec/dmarc_inspector_spec.rb
@@ -1,7 +1,17 @@
 describe DMARCInspector do
   describe '#receiver_policy' do
-    it 'returns :none' do
-      expect(DMARCInspector.new.receiver_policy).to eq(:none)
+    it 'returns receiver policy of a domain' do
+      # TODO: Mock DNS query
+      dmarc_inspector = DMARCInspector.new('gmail.com')
+      expect(dmarc_inspector.receiver_policy).to eq(:none)
+    end
+
+    context "policy is 'reject'" do
+      it 'returns :reject' do
+        # TODO: Mock DNS query
+        dmarc_inspector = DMARCInspector.new('yahoo.com')
+        expect(dmarc_inspector.receiver_policy).to eq(:reject)
+      end
     end
   end
 end
